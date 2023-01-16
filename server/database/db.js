11f8e30a56f76d810fa1f94709225e78f5e29eed
Abu Obaidah <abuobaidah00@gmail.com>
2023-01-16 19:19:07 +0530

    mongodb compass server auto start and export data to excel

--- server/database/db.js
+++ server/database/db.js
@@ -6,16 +6,6 @@ const Connection = async () => {
         .then(() => console.log('Successfully connected to MongoDB Compass'))
         .catch(error => console.error('Error while connecting to MongoDB Compass:', error));
 
-    // const URL = `mongodb+srv://${username}:${password}@cluster0.crdnocb.mongodb.net/person_info?retryWrites=true&w=majority`;
-
-    // try {
-    //     await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true })
-    //     console.log("connected successfully to database");
-    // }
-    // catch (error) {
-    //     console.log("error while connecting to database", error)
-    // }
-
 }
 
 export default Connection;
